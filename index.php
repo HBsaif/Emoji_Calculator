bb75a42bddf6fbc591ba59221ddae929e9aacc32
Hasanul Banna Saif <saifbanna79@gmail.com>
2022-07-02 20:14:09 +0600

    Checking the input fields empty or not updated

--- index.php
+++ index.php
@@ -74,19 +74,22 @@
           include './includes/class-autoload.php';
 
           // Get the numbers and operator value from form
-          $num1 = $_POST['num1'];
-          $num2 = $_POST['num2'];
-          $operator = $_POST['operator'];
+          if(isset($_POST['num1']) && isset($_POST['num2'])){
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+            $operator = $_POST['operator'];
 
-          // Create an instance of Clac class
-          $calc = new Calc($num1, $num2, $operator);
+            // Create an instance of Clac class
+            $calc = new Calc($num1, $num2, $operator);
 
-          try {
-              // Print the inputs and output
-              echo $num1." ".$operator." ".$num2." = ". $calc->calculator();
-          } catch (TypeError $e) {
-              echo "Error!: " .$e->getMessage();
+            try {
+                // Print the inputs and output
+                echo $num1." ".$operator." ".$num2." = ". $calc->calculator();
+            } catch (TypeError $e) {
+                echo "Error!: " .$e->getMessage();
+            }
           }
+          
 
           ?>
       </div>
